06c9c776cf99bc21298db6bfa4d766b30d268de3
Gupta, Sunny <sunny.gupta@united.com>
2024-02-23 23:50:50 +0530

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { A11yModule } from '@angular/cdk/a11y';
+import {DragDropModule} from '@angular/cdk/drag-drop';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +15,8 @@ import { BasicComponent } from './basic/basic.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    A11yModule
+    A11yModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
